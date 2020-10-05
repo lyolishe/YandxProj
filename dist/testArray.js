e59b4bd968249443adc20098775ae4de3e2151f4
ButtSurfer <fisher10058@mail.ru>
2020-10-05 17:02:03 +0300

    Добавил класс MyArray - создание массиво-подобного объекта с имитацией выделения памяти

--- dist/testArray.js
+++ dist/testArray.js
@@ -1,25 +1,38 @@
 class MyArray {
   constructor(initialSize) {
-    if (!(initialSize >= 0) || !initialSize) {
+    if (initialSize === undefined || initialSize < 0) {
       throw new Error('Размер массива должен быть больше нуля');
     }
 
-    this.size = log(initialSize);
+    this.size = initialSize;
     this.memory = allocate(initialSize);
-    log("here")
+
     this.length = 0;
   }
 
+
+  checkIndex(index) {
+    if (index!== undefined && (index >= 0 && index <= this.length)){
+      return true
+    } else {
+      throw new Error("Некорректный индекс массива")
+    }
+  }
   // Возвращает значение по индексу.
   // Если индекс за пределами — кидает ошибку.
   get(index){
-    //
+    if (this.checkIndex(index)) {
+      return this.memory[index];
+    }
   }
+
 
   // Устанавливает значение по индексу.
   // Если индекс за пределами — кидает ошибку.
   set(index, value) {
-    //
+    if (this.checkIndex(index)) {
+      this.memory[index] = value;
+    }
   }
 
   // Добавляет новый элемент в массив.
@@ -30,19 +43,53 @@ class MyArray {
   // Увеличивает выделенную память вдвое, если необходимо.
   // Возвращает новую длину массива.
   add(value, index) {
-    //
+    if (this.checkIndex(index? index : this.length)) {
+
+      if (this.length === this.size) {
+        this.size = 2 * this.size
+        //Выделяю НОВУЮ область памяти
+        const newMemory = allocate(this.size)
+        //Копирую старые значения в новую область мамяти
+        Object.keys(this.memory).forEach(i => {
+          newMemory[i] = this.memory[i];
+        });
+        this.memory = newMemory
+      }
+
+      if(index !== undefined) {
+        //если есть индекс, смотрю что он затирает ли он что-то
+        const replaced = this.memory[index]
+
+        if (replaced !== undefined) {
+          for (let i = this.length+1; i !== index; i--) {
+            this.memory[i] = this.memory[i-1];
+          }
+        }
+        this.memory[index] = value;
+      } else {
+        //индекса нет - пишу в конец массива
+        this.memory[this.length] = value
+      }
+      this.length++
+    }
+    return this.length
   }
 
   // Удаляет элемент по индексу со сдвигом всех последующих элементов.
   // Если индекс за пределами - кидает ошибку.
   // Возвращает новую длину массива.
   delete(index) {
-    //
+    if (this.checkIndex(index) && this.memory[index] !== undefined) {
+      //если есть индекс, и он хорош - перетираю каждое значение, начиная с него, значением с права
+      for (let i = index; i!== this.length; i++) {
+        this.memory[i] = this.memory[i+1];
+      }
+      //последнее перетираю ручками
+      this.memory[this.length] = undefined;
+      this.length--
+    }
+    return this.length
   }
-}
-function log(val) {
-  console.log(val);
-return val
 }
 
 function allocate(size) {
@@ -54,3 +101,5 @@ function allocate(size) {
 
   return memory;
 }
+const newOne = new MyArray(5)
+console.log(newOne)
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const PORT = 3000;
 
 app.use(express.static('./dist'));
 
+app.get('/testArray.js', function (req, res) {
+  res.send(`${_dirname}/testArray.js`)
+})
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
 }); 
